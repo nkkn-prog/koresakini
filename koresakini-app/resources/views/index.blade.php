5111142a1665fd2787dc5002324cc910f54cf408
安藤直城 <andonaoki@Macbook-pro-8.local>
2023-02-06 23:56:31 +0900

    install bootstrap & add index and create page

--- koresakini-app/resources/views/index.blade.php
+++ koresakini-app/resources/views/index.blade.php
@@ -4,9 +4,12 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="{{asset('css/index.css')}}" rel="stylesheet">
   <title>Hello</title>
 </head>
 <body>
-  <div>Hello! Welcome to コレサキニ！</div>
+  <div id="logo">コレサキニ</div>
+  <div class="first-msg">Hello! Welcome to コレサキニ！</div>
+  <a href="/create">タスクを作成する</a>
 </body>
 </html>
